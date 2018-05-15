c773c220a572c95fefdab76be0eb9c3b05b39924
ark120202 <ark120202@gmail.com>
2018-05-16 04:07:47 +0500

    feat(babel-plugin-lua-generator): wrap not-last return statements with block statement

--- packages/babel-plugin-lua-generator/src/index.js
+++ packages/babel-plugin-lua-generator/src/index.js
@@ -2,12 +2,21 @@ import generate from 'lua-generator';
 import transform from './transform';
 
 const FOR_OF_UID = Symbol.for('for..of uid');
-export default function() {
+export default function({ types: t }) {
   return {
     visitor: {
       ForOfStatement(path) {
         const uid = path.scope.generateUidIdentifier('');
         path.node[FOR_OF_UID] = uid;
+      },
+
+      ReturnStatement(path) {
+        if (
+          path.getNextSibling().node != null &&
+          (!path.parentPath.isBlockStatement() || path.parent.body.length > 1)
+        ) {
+          path.replaceWith(t.blockStatement([path.node]));
+        }
       },
     },
 
